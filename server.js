2ba3797f850bae2af4a45320d117996af4ed8abb
Janis Krasemann <krasemann.janis@gmail.com>
2016-07-11 21:52:53 +0000

    Implement GET :timestamp logic and create all helper functions

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const app = express();
 
+var isNaturalLanguageDate = function (candidate) {
+    return false;
+}
+
+var isUnixDate = function (candidate) {
+    return false;
+}
+
+var createNaturalDate = function (date) {
+    return new Date(date);
+}
+
+var createUnixTimestamp = function (date) {
+    return new Date(date);
+}
+
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.send('Timestamp Microservice - Do a GET call with a timestamp parameter to try it!');
 });
 
-app.listen(8080, function () {
-   console.log('Server running on port 8080'); 
+app.get('/:timestamp', function (req, res) {
+    var unixDate, naturalDate;
+    var t = req.params.timestamp;
+    if (isNaturalLanguageDate(t)) {
+        naturalDate = t;
+        unixDate = createUnixTimestamp(t);
+    } else if (isUnixDate(t)) {
+        unixDate = t;
+        naturalDate = createNaturalDate(t);
+    } else {
+        unixDate = naturalDate = null;
+    }
+    res.send({ unix: unixDate, natural: naturalDate });
+});
+
+app.listen(process.env.PORT, function () {
+   console.log('Timestamp Microservice running on port ' + process.env.PORT); 
 });
